a8997250cf41e11ba16701094ded04f9785484ac
Dmitriy <devilins24@gmail.com>
2021-12-20 21:16:21 +0200

    add mobile menu nav toggle

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuIcon = document.querySelector(".hamburger-menu");
+const menu = document.querySelector(".menu");
+
+menuIcon.addEventListener("click", () => {
+  menu.classList.toggle("show");
+});
